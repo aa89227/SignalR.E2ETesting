f2af63a6662e19f2ee2ed3f00fa4b721f787aaea
aa89227 <aa89227@gmail.com>
2023-10-14 14:09:49 +0800

    try fix with response compression

--- Example/ExampleWebApplication/Program.cs
+++ Example/ExampleWebApplication/Program.cs
@@ -6,8 +6,12 @@ builder.Services.AddSignalR(options =>
 {
     options.EnableDetailedErrors = true;
 });
-
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+});
 var app = builder.Build();
+app.UseResponseCompression();
 app.MapHub<ExampleHub>("/examplehub");
 
 app.Run();
