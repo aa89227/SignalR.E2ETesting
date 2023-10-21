18a552c854604869e61e7683d229637c4952cb64
aa89227 <aa89227@gmail.com>
2023-10-21 23:44:27 +0800

    remove response compression

--- Example/ExampleWebApplication/Program.cs
+++ Example/ExampleWebApplication/Program.cs
@@ -6,12 +6,7 @@ builder.Services.AddSignalR(options =>
 {
     options.EnableDetailedErrors = true;
 });
-builder.Services.AddResponseCompression(options =>
-{
-    options.EnableForHttps = true;
-});
 var app = builder.Build();
-app.UseResponseCompression();
 app.MapHub<ExampleHub>("/examplehub");
 
 app.Run();
